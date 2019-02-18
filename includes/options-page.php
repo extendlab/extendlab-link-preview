594308fc57ef9e22dce1b6f7ef0053574facf8c8
Fabian Pecher <f.pecher@sdv-werbestudio.de>
2019-02-18 16:15:29 +0100

    changed style of settings page

--- includes/options-page.php
+++ includes/options-page.php
@@ -11,8 +11,8 @@ if ( is_admin() ){
 function extlbRegisterMenuEntry() {
 	// add_options_page( 'Link Preview Settings', 'Link Preview', 'switch_themes', 'extlb-options-page', 'extlbOptionsPage', $icon_url, $position );
 
-	$page_title = 'Link Preview Settings';
-	$menu_title = 'Link Preview';
+	$page_title = 'Extendlab - Link Preview Settings';
+	$menu_title = 'Extendlab - Link Preview';
 	$capability = 'switch_themes';
 	$menu_slug = 'extlb-options-page';
 	$function = 'extlbOptionsPage';
@@ -29,23 +29,65 @@ function extlbOptionsPage() {
 	?>
 
 	<div class="wrap">
-		<h1>Link Preview Settings</h1>
+		<h1>Extendlab - Link Preview Settings</h1>
 		<form method="post" action="options.php">
 			<?php settings_fields( 'extlb-options' ); ?>
 			<?php do_settings_sections( 'extlb-options' ); ?>
+			<table class="form-table">
+				<tbody>
+					<tr>
+						<th scope="row">Appearance</th>
+						<td>
+							<fieldset>
+								<label for="extlb_darkmode">
+										<input type="checkbox" id="extlb_darkmode" name="extlb_darkmode" <?php echo (get_option('extlb_darkmode') == 'on') ? 'checked' : 'false'; ?>/>
+										Darkmode on
+								</label>
+								<p class="description">You can choose if you want a dark grey or a white popup</p>
+							</fieldset>
+						</td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td>
+							<fieldset>
+								<label for="extlb_hide_thumbnails">
+										<input type="checkbox" id="extlb_hide_thumbnails" name="extlb_hide_thumbnails"<?php echo (get_option('extlb_hide_thumbnails') == 'on') ? 'checked' : 'false'; ?>/>
+										Hide Thumbnails
+								</label>
+								<p class="description">You can toggle the visibility of the article thumbnails inside the popup</p>
+							</fieldset>
+						</td>
+					</tr>
+					<tr>
+						<th scope="row">Visibility</th>
+						<td>
+							<fieldset>
+								<label for="extlb_disable_mobile">
+										<input type="checkbox" id="extlb_disable_mobile" name="extlb_disable_mobile"<?php echo (get_option('extlb_disable_mobile') == 'on') ? 'checked' : 'false'; ?>/>
+										Hide on mobile (< 720px)
+								</label>
+								<p class="description">You can choose if you want to deactivate the popup on mobile devices</p>
+							</fieldset>
+						</td>
+					</tr>
 
-			<label for="extlb_darkmode">Darkmode?</label>
-			<input type="checkbox" id="extlb_darkmode" name="extlb_darkmode" <?php echo (get_option('extlb_darkmode') == 'on') ? 'checked' : 'false'; ?>/>
-			<br><br>
-			<label for="extlb_disable_mobile">Disable on mobile (< 720px)?</label>
-			<input type="checkbox" id="extlb_disable_mobile" name="extlb_disable_mobile"<?php echo (get_option('extlb_disable_mobile') == 'on') ? 'checked' : 'false'; ?>/>
-			<br><br>
-			<label for="extlb_hide_thumbnails">Hide Thumbnails?</label>
-			<input type="checkbox" id="extlb_hide_thumbnails" name="extlb_hide_thumbnails"<?php echo (get_option('extlb_hide_thumbnails') == 'on') ? 'checked' : 'false'; ?>/>
-			<br><br>
-			<label for="extlb_link_selector">Selector <span>Default is .entry-content a</span></label>
-			<input type="text" id="extlb_link_selector" name="extlb_link_selector" value="<?php echo esc_attr( get_option('extlb_link_selector') ); ?>" />
+					<tr>
+						<th scope="row">Selector</th>
+						<td>
+							<fieldset>
+								<label for="extlb_link_selector">
+										<input type="text" id="extlb_link_selector" name="extlb_link_selector" value="<?php echo esc_attr( get_option('extlb_link_selector') ); ?>" />
+										Your custom css selector
+								</label>
+								<p class="description">Add your custom CSS selector here, the default is .entry-content a</p>
+							</fieldset>
+						</td>
+					</tr>
+				</tbody>
 
+			</table>
+	
 			<?php submit_button(); ?>
 		</form>
 	</div>
